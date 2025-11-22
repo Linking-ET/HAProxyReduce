28ecbf5e8b18587f1501d6221b69afaaefde72a8
Jones <admin@jonesdev.xyz>
2025-11-22 10:58:17 +0100

    Bump velocity api annotation processor to 3.4.0-SNAPSHOT

--- velocity/build.gradle.kts
+++ velocity/build.gradle.kts
@@ -10,7 +10,7 @@ dependencies {
     implementation("io.netty:netty-codec-haproxy:4.1.79.Final")
     implementation("org.bstats:bstats-velocity:3.0.2")
 
-    annotationProcessor("com.velocitypowered:velocity-api:3.3.0-SNAPSHOT")
+    annotationProcessor("com.velocitypowered:velocity-api:3.4.0-SNAPSHOT")
     implementation(kotlin("stdlib-jdk8"))
 }
 
